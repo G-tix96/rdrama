fac797006e973cfad5359ca238462bace21d4858
Aevann1 <randomname42029@gmail.com>
2022-05-10 14:14:09 +0200

    sf

--- files/helpers/const.py
+++ files/helpers/const.py
@@ -131,6 +131,7 @@ if SITE in {'rdrama.net','devrama.xyz'}:
 	AUTOCHOICE_ID = 9167
 	BASEDBOT_ID = 0
 
+	SCHIZO_ID = 8494
 	A_ID = 1230
 	KIPPY_ID = 7150
 	GIFT_NOTIF_ID = 995
@@ -174,6 +175,7 @@ elif SITE == "pcmemes.net":
 	AUTOCHOICE_ID = 2072
 	BASEDBOT_ID = 800
 
+	SCHIZO_ID = 0
 	A_ID = 0
 	KIPPY_ID = 1592
 	PIZZASHILL_ID = 0
@@ -206,6 +208,7 @@ elif SITE == 'cringetopia.org':
 	AUTOCHOICE_ID = 8
 	BASEDBOT_ID = 0
 
+	SCHIZO_ID = 0
 	A_ID = 0
 	KIPPY_ID = 0
 	GIFT_NOTIF_ID = 43
@@ -687,6 +690,8 @@ NOTIFIED_USERS = {
 	'kippy': KIPPY_ID,
 	'the_homocracy': HOMO_ID,
 	'soren': SOREN_ID,
+	'schizocel': SCHIZO_ID,
+	'scitzocel': SCHIZO_ID
 }
 
 FORTUNE_REPLIES = ('<b style="color:#6023f8">Your fortune: Allah Wills It</b>','<b style="color:#d302a7">Your fortune: Inshallah, Only Good Things Shall Come To Pass</b>','<b style="color:#e7890c">Your fortune: Allah Smiles At You This Day</b>','<b style="color:#7fec11">Your fortune: Your Bussy Is In For A Blasting</b>','<b style="color:#43fd3b">Your fortune: You Will Be Propositioned By A High-Tier Twink</b>','<b style="color:#9d05da">Your fortune: Repent, You Have Displeased Allah And His Vengeance Is Nigh</b>','<b style="color:#f51c6a">Your fortune: Reply Hazy, Try Again</b>','<b style="color:#00cbb0">Your fortune: lmao you just lost 100 coins</b>','<b style="color:#2a56fb">Your fortune: Yikes 😬</b>','<b style="color:#0893e1">Your fortune: You Will Be Blessed With Many Black Bulls</b>','<b style="color:#16f174">Your fortune: NEETmax, The Day Is Lost If You Venture Outside</b>','<b style="color:#fd4d32">Your fortune: A Taste Of Jannah Awaits You Today</b>','<b style="color:#bac200">Your fortune: Watch Your Back</b>','<b style="color:#6023f8">Your fortune: Outlook good</b>','<b style="color:#d302a7">Your fortune: Godly Luck</b>','<b style="color:#e7890c">Your fortune: Good Luck</b>','<b style="color:#7fec11">Your fortune: Bad Luck</b>','<b style="color:#43fd3b">Your fortune: Good news will come to you by mail</b>','<b style="color:#9d05da">Your fortune: Very Bad Luck</b>','<b style="color:#00cbb0">Your fortune: ｷﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━ !!!!</b>','<b style="color:#2a56fb">Your fortune: Better not tell you now</b>','<b style="color:#0893e1">Your fortune: You will meet a dark handsome stranger</b>','<b style="color:#16f174">Your fortune: （　´_ゝ`）ﾌｰﾝ</b>','<b style="color:#fd4d32">Your fortune: Excellent Luck</b>','<b style="color:#bac200">Your fortune: Average Luck</b>')
