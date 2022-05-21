20cc20d806c71f4cd4c36947cb5b33ed2b4311dd
Aevann1 <randomname42029@gmail.com>
2022-05-21 14:36:22 +0200

    fsd

--- files/classes/user.py
+++ files/classes/user.py
@@ -188,7 +188,8 @@ class User(Base):
 	@property
 	@lazy
 	def is_cakeday(self):
-		if SITE_NAME != 'rDrama': return False
+		if SITE_NAME == 'rDrama': return True
+		else: return False
 		if time.time() - self.created_utc > 363 * 86400:
 			date = time.strftime("%d %b", time.gmtime(self.created_utc))
 			now = time.strftime("%d %b", time.gmtime())
