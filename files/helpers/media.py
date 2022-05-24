67b4f13d98fd7c55216251c68e7020ba1555d308
Aevann1 <randomname42029@gmail.com>
2022-05-24 22:07:04 +0200

    webm

--- files/helpers/media.py
+++ files/helpers/media.py
@@ -10,7 +10,7 @@ from .const import *
 
 
 def process_audio(file):
-	name = f'/audio/{time.time()}'.replace('.','') + '.' + file.filename.split('.')[-1]
+	name = f'/audio/{time.time()}'.replace('.','') + '.' + file.content_type.split('/')[1]
 	file.save(name)
 
 	if SITE_NAME == 'WPD' or os.stat(name).st_size > 8 * 1024 * 1024:
@@ -23,21 +23,26 @@ def process_audio(file):
 
 
 def process_video(file):
-	name = f'/videos/{time.time()}'.replace('.','')
-	file.save(name)
+	old = f'/videos/{time.time()}'.replace('.','')
+	file.save(old)
 
-	os.system(f'ffmpeg -y -loglevel warning -i {name} -map_metadata -1 -c:v copy -c:a copy {name}.mp4')
-	os.remove(name)
-	name += '.' + file.filename.split('.')[-1]
+	extension = file.content_type.split('/')[1]
+	new = old + '.' + extension
 
-	size = os.stat(name).st_size
-	if SITE_NAME == 'WPD' or os.stat(name).st_size > 8 * 1024 * 1024:
-		with open(name, 'rb') as f:
-			os.remove(name)
+	if extension == 'webm':
+		os.rename(old, new)
+	else:
+		os.system(f'ffmpeg -y -loglevel warning -i {old} -map_metadata -1 -c:v copy -c:a copy {new}')
+		os.remove(old)
+
+	size = os.stat(new).st_size
+	if SITE_NAME == 'WPD' or os.stat(new).st_size > 8 * 1024 * 1024:
+		with open(new, 'rb') as f:
+			os.remove(new)
 			req = requests.request("POST", "https://pomf2.lain.la/upload.php", files={'files[]': f}, timeout=20).json()
 		return req['files'][0]['url']
 
-	return f'{SITE_FULL}{name}'
+	return f'{SITE_FULL}{new}'
 
 
 def process_image(patron, filename=None, resize=0):
