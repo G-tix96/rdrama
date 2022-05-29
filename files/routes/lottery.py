cf7ee615e6d191fdf6488496e900d687c3bb4e77
Outrun Colors, LLC <itsoutruncolors@gmail.com>
2022-05-28 19:02:35 -0500

    Add skeleton for route interactions

--- files/routes/lottery.py
+++ files/routes/lottery.py
@@ -4,6 +4,25 @@ from files.helpers.alerts import *
 from files.helpers.get import *
 from files.helpers.const import *
 
+
+@app.post("/lottery/start")
+@auth_required
+def lottery_start(v):
+    # Save changes
+    g.db.commit()
+
+    return {"message": "Lottershe started."}
+
+
+@app.post("/lottery/end")
+@auth_required
+def lottery_end(v):
+    # Save changes
+    g.db.commit()
+
+    return {"message": "Lottershe ended."}
+
+
 @app.post("/lottery/buy")
 @limiter.limit("1/second;30/minute;200/hour;1000/day")
 @auth_required
@@ -14,7 +33,17 @@ def lottery_buy(v):
     # Charge user for ticket
     v.coins -= 12
 
+    # Check for active lottery
+    pass
+
     # Save changes
     g.db.commit()
 
-    return {"message": "Lottershe ticket purchased!"}
+    return {"message": "Lottershe ticket purchased!", "stats": {"sessionEnds": 0, "prize": 0, "ticketsSoldSession": 0, "ticketsSoldTotal": 0, "ticketsHeldSession": 0, "ticketsHeldTotal": 0, "totalWinnings": 0}}
+
+
+@app.get("/lottery/stats")
+@limiter.limit("1/second;30/minute;200/hour;1000/day")
+@auth_required
+def lottery_stats(v):
+    return {"message": {"sessionEnds": 0, "prize": 0, "ticketsSoldSession": 0, "ticketsSoldTotal": 0}}
