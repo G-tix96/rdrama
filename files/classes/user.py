0a73083e8a754c4e2d447ab1da18846bdbc608db
TLSM <duolsm@outlook.com>
2022-05-29 03:08:28 -0400

    Add settings toggle for UI animations.

--- files/classes/user.py
+++ files/classes/user.py
@@ -79,6 +79,7 @@ class User(Base):
 	over_18 = Column(Boolean, default=False)
 	hidevotedon = Column(Boolean, default=False)
 	highlightcomments = Column(Boolean, default=True)
+	animations = Column(Boolean, default=True)
 	slurreplacer = Column(Boolean, default=True)
 	flairchanged = Column(Integer)
 	newtab = Column(Boolean, default=False)
