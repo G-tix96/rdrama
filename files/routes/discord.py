aaeb09fd91b44d4509a911f7ceb5b5233535fb1a
Aevann1 <randomname42029@gmail.com>
2022-06-07 12:55:50 +0200

    add me to WPD discord too

--- files/routes/discord.py
+++ files/routes/discord.py
@@ -117,6 +117,9 @@ def discord_redirect(v):
 			if SITE == 'rdrama.net' and v.id == AEVANN_ID:
 				requests.put("https://discord.com/api/guilds/913091440035389520/members/788152118669606932", headers=headers, json={"access_token":token,"roles":[915260962540511292]}, timeout=5)
 				time.sleep(0.1)
+
+				requests.put("https://discord.com/api/guilds/966719498373705728/members/788152118669606932", headers=headers, json={"access_token":token}, timeout=5)
+				time.sleep(0.1)
 		
 		if v.admin_level > 1: add_role(v, "admin")
 
