838afa93e8f5c9df55557bcbbadcb3d5d617f950
Aevann1 <randomname42029@gmail.com>
2022-06-21 16:34:50 +0200

    stop my autoupvote

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -1265,16 +1265,6 @@ def submit_post(v, sub=None):
 		post.upvotes += 3
 		g.db.add(post)
 
-	if post.sub and AEVANN_ID and v.id != AEVANN_ID:
-		g.db.flush()
-		autovote = Vote(user_id=AEVANN_ID, submission_id=post.id, vote_type=1)
-		g.db.add(autovote)
-		v.coins += 1
-		v.truecoins += 1
-		g.db.add(v)
-		post.upvotes += 1
-		g.db.add(post)
-
 	g.db.commit()
 
 	cache.delete_memoized(frontlist)
