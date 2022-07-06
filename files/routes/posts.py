742a3698ea217b982abc927ad4be68f728c6c2e3
Aevann1 <randomname42029@gmail.com>
2022-07-06 13:00:28 +0200

    fix my vote being above OP's vote

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -1201,13 +1201,17 @@ def submit_post(v, sub=None):
 
 	if request.host == 'rdrama.net' and post.sub and post.sub not in ('dankchristianmemes','fatpeoplehate','braincels','truth') and v.id != AEVANN_ID:
 		g.db.flush()
-		autovote = Vote(user_id=AEVANN_ID, submission_id=post.id, vote_type=1)
+		autovote = Vote(
+			user_id=AEVANN_ID,
+			submission_id=post.id,
+			vote_type=1
+		)
+		autovote.created_utc += 1
 		g.db.add(autovote)
 		v.coins += 1
 		v.truecoins += 1
 		g.db.add(v)
 		post.upvotes += 1
-		post.created_utc += 1
 		g.db.add(post)
 
 	cache.delete_memoized(frontlist)
