0ee6566c21759fb9911fcb98af7496fc184dad8a
Aevann1 <randomname42029@gmail.com>
2022-07-11 20:43:44 +0200

    tweak spider exclusion

--- files/helpers/wrappers.py
+++ files/helpers/wrappers.py
@@ -61,7 +61,7 @@ def get_logged_in_user():
 			g.db.add(v)
 	else:
 		ua = str(user_agents.parse(g.agent))
-		if not ua.startswith('Spider') and 'bot' not in ua.lower():
+		if 'spider' not in ua.lower() and 'bot' not in ua.lower():
 			loggedout[session["session_id"]] = (timestamp, ua)
 	
 	g.loggedin_counter = len([x for x in loggedin.values() if timestamp-x < LOGGEDIN_ACTIVE_TIME])
