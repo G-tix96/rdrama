e68c2efb1b17bc25970c865cd622f22a54fc8cca
scitzocel <98491720+scitzocel@users.noreply.github.com>
2022-08-05 05:52:03 -0400

    sneed (#328)
    * sneed
    
    * schizosneed
    
    soren
    
    * Update posts.py

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -1041,11 +1041,39 @@ def submit_post(v, sub=None):
 		g.db.add(v)
 		post.upvotes += 3
 		g.db.add(post)
+	
+	if v.id == SOREN_ID:
+		autovote = Vote(
+			user_id=SCHIZO_ID,
+			submission_id=post.id,
+			vote_type=-1
+		)
+		g.db.add(autovote)
+		v.coins += 1
+		v.truecoins += 1
+		g.db.add(v)
+		post.downvotes += 1
+		g.db.add(post)
 
 	if SITE == 'rdrama.net' and post.sub and post.sub not in ('dankchristianmemes','fatpeoplehate','braincels','truth') and v.id != AEVANN_ID:
 		g.db.flush()
 		autovote = Vote(
 			user_id=AEVANN_ID,
+			submission_id=post.id,
+			vote_type=1
+		)
+		autovote.created_utc += 1
+		g.db.add(autovote)
+		v.coins += 1
+		v.truecoins += 1
+		g.db.add(v)
+		post.upvotes += 1
+		g.db.add(post)
+	
+	if SITE == 'rdrama.net' and post.sub and post.sub in ('stupidpol') and v.id not in (SOREN_ID, SCHIZO_ID):
+		g.db.flush()
+		autovote = Vote(
+			user_id=SCHIZO_ID,
 			submission_id=post.id,
 			vote_type=1
 		)
