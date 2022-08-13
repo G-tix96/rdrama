484cac64f0c503a36e23fe5ace608ab42fbe6572
Aevann1 <randomname42029@gmail.com>
2022-08-13 10:35:44 +0200

    fix 500 errors

--- files/classes/comment.py
+++ files/classes/comment.py
@@ -19,7 +19,7 @@ from math import floor
 
 def normalize_urls_runtime(body, v):
 	
-	if v.reddit != 'old.reddit.com':
+	if v and v.reddit != 'old.reddit.com':
 		body = reddit_to_vreddit_regex.sub(rf'\1https://{v.reddit}/\2/', body)
 		if v.nitter: body = twitter_to_nitter_regex.sub(r'https://nitter.42l.fr/\1', body)
 
