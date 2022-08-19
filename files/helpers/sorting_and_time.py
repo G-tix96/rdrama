df2f5d3b617b2d01872ea176b9b3fe44082a1a50
Aevann1 <randomname42029@gmail.com>
2022-08-19 14:50:35 +0200

    restore downvotes acting like upvotes in rdrama for comments

--- files/helpers/sorting_and_time.py
+++ files/helpers/sorting_and_time.py
@@ -30,6 +30,8 @@ def sort_comments(sort, comments):
 		return comments.order_by((Comment.upvotes+1)/(Comment.downvotes+1) + (Comment.downvotes+1)/(Comment.upvotes+1), Comment.downvotes.desc(), Comment.id.desc())
 	elif sort == "bottom":
 		return comments.order_by(Comment.upvotes - Comment.downvotes)
+	elif SITE_NAME == 'rDrama':
+		return comments.order_by(Comment.realupvotes.desc(), Comment.id.desc())
 	else:
 		return comments.order_by(Comment.downvotes - Comment.upvotes, Comment.id.desc())
 
