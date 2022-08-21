f7356c0843801d48bb0d0e9d1e03f5494fa731bd
Aevann1 <randomname42029@gmail.com>
2022-08-21 19:08:48 +0200

    censor post titles in /log

--- files/classes/mod_logs.py
+++ files/classes/mod_logs.py
@@ -5,6 +5,7 @@ import time
 from files.helpers.lazy import lazy
 from copy import deepcopy
 from files.helpers.const import *
+from files.helpers.regex import censor_slurs
 
 class ModAction(Base):
 	__tablename__ = "modactions"
@@ -89,7 +90,7 @@ class ModAction(Base):
 		if self.target_user: return f'<a href="{self.target_user.url}">{self.target_user.username}</a>'
 		elif self.target_post:
 			if self.target_post.club: return f'<a href="{self.target_post.permalink}">{CC} ONLY</a>'
-			return f'<a href="{self.target_post.permalink}">{self.target_post.title_html}</a>'
+			return censor_slurs(f'<a href="{self.target_post.permalink}">{self.target_post.title_html}</a>', None)
 		elif self.target_comment_id: return f'<a href="/comment/{self.target_comment_id}?context=8#context">comment</a>'
 
 	@property
