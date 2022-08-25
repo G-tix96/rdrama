3bff9489da23825630938401e05f67550ab8f452
Aevann1 <randomname42029@gmail.com>
2022-08-25 19:57:15 +0200

    exclude paypig badges from badge_grant

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -531,7 +531,7 @@ def badge_grant_post(v):
 	try: badge_id = int(request.values.get("badge_id"))
 	except: abort(400)
 
-	if badge_id in {16,17,94,95,96,97,98,109,137} and v.id != AEVANN_ID:
+	if badge_id in {16,17,21,22,23,24,25,26,27,94,95,96,97,98,109,137} and v.id != AEVANN_ID:
 		abort(403)
 
 	if user.has_badge(badge_id):
