21fc6fe19c568352b880595caa6b3560ece70791
Aevann1 <randomname42029@gmail.com>
2022-08-26 20:30:52 +0200

    dude testing on prod lmao

--- files/assets/js/chat.js
+++ files/assets/js/chat.js
@@ -179,7 +179,7 @@ socket.on('typing', function (users){
 function scroll_chat() {
     setTimeout(function () {
 		box.scrollTo(0, box.scrollHeight)
-    }, 0500);
+    }, 0300);
 }
 
 scroll_chat()
