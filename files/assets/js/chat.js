750f045776ae63dc1fde4f202cb30ffb84d3de55
Aevann1 <randomname42029@gmail.com>
2022-08-26 20:32:25 +0200

    scroll chat when textarea is clicked

--- files/assets/js/chat.js
+++ files/assets/js/chat.js
@@ -179,7 +179,7 @@ socket.on('typing', function (users){
 function scroll_chat() {
     setTimeout(function () {
 		box.scrollTo(0, box.scrollHeight)
-    }, 0100);
+    }, 0200);
 }
 
 scroll_chat()
