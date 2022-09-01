523bfd93c5afcfea2c8768c0a2593f02ca4284b3
Aevann1 <randomname42029@gmail.com>
2022-09-01 22:43:11 +0200

    show first frame of video

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -319,8 +319,8 @@ def sanitize(sanitized, edit=False, limit_pings=0, showmore=True):
 
 		sanitized = sanitized.replace(i.group(0), htmlsource)
 
-	sanitized = video_sub_regex.sub(r'\1<video controls preload="none"><source src="\2"></video>', sanitized)
-	sanitized = audio_sub_regex.sub(r'\1<audio controls preload="none" src="\2"></audio>', sanitized)
+	sanitized = video_sub_regex.sub(r'\1<video controls preload="metadata"><source src="\2"></video>', sanitized)
+	sanitized = audio_sub_regex.sub(r'\1<audio controls preload="metadata" src="\2"></audio>', sanitized)
 
 	if not edit:
 		for marsey in g.db.query(Marsey).filter(Marsey.name.in_(marseys_used)).all():
