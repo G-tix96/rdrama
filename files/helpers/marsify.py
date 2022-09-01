c4e46839c86d141ecd10b2d4661d2f4e8ace2119
Aevann1 <randomname42029@gmail.com>
2022-09-02 00:47:09 +0200

    reduce minimum char count for marsify from 5 to 4

--- files/helpers/marsify.py
+++ files/helpers/marsify.py
@@ -6,7 +6,7 @@ def marsify(text):
 	for x in text.split(' '):
 		new_text += f'{x} '
 		x = x.lower()
-		if len(x) > 4 and x in marsey_mappings:
+		if len(x) > 3 and x in marsey_mappings:
 			marsey = choice(marsey_mappings[x])
 			new_text += f':{marsey}: '
 	return new_text
