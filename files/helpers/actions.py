284dfbc30ede95d2f23e6f9575b5b44080a1090c
Aevann1 <randomname42029@gmail.com>
2022-09-03 16:24:16 +0200

    ignore archiveorg errors

--- files/helpers/actions.py
+++ files/helpers/actions.py
@@ -29,7 +29,8 @@ def badge_grant(user, badge_id, description=None, url=None):
 
 
 def archiveorg(url):
-	x = requests.get(f'https://web.archive.org/save/{url}', headers={'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}, timeout=100)
+	try: requests.get(f'https://web.archive.org/save/{url}', headers={'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}, timeout=100)
+	except: pass
 
 def archive_url(url):
 	if url.startswith(SITE_FULL): return
