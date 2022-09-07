d695755aa240506503dc9e30dbfba8e9180eae12
Aevann1 <randomname42029@gmail.com>
2022-09-07 03:00:02 +0000

    sneed

--- schema.sql
+++ schema.sql
@@ -2592,6 +2592,9 @@ COPY public.badge_defs (id, name, description) FROM stdin;
 163	Marsey Jacobs	Designed 10 hats!
 166	Giorgio Armarsey	Designed 250 hats 😲
 164	Marsey de Givenchy	Designed 50 hats, holy cap.
+167	Marsified	User’s comments are marsified for the next six (6) hours
+169	Early Life Checked	Shut it down! The Early Life section of your Wikipedia bio is on full display for 24 hours.
+170	Emojified	This user’s 💻 posts 📫 are emojified 😀 for the next ⏭ six 6️⃣ hours ⏳
 4	White Hat	Discreetly reported an exploit
 1	Alpha User	Joined during open alpha
 2	Verified Email	Verified Email
@@ -2671,7 +2674,9 @@ COPY public.badge_defs (id, name, description) FROM stdin;
 159	It's Over	Lost a 100,000 dramacoin bet. It's fucking over.
 161	Pro Goombler	Won a 10,000 dramacoin bet. Some would say to quit while you're ahead, but they didn't just win 10k - keep at it!
 165	Marsey Chanel	Designed 100 hats!!!
+168	BITTEN!	This user has been forcibly recruited to House Vampire for 24 hours
 28	Chud	Marked as a chud
+171	Rainbowed	This user has super cute text for 24 hours.
 59	Lolcow	Beautiful and valid milk provider
 25	Marsey's Sugar Daddy	Contributed at least $100
 26	JIDF Bankroller	Donated at least $250
@@ -2698,7 +2703,7 @@ COPY public.badge_defs (id, name, description) FROM stdin;
 -- Name: badge_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.badge_defs_id_seq', 166, true);
+SELECT pg_catalog.setval('public.badge_defs_id_seq', 171, true);
 
 
 --
