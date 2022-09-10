744f847c738fab74f2c33c6d3480a50eb74b9eef
Aevann1 <randomname42029@gmail.com>
2022-09-10 02:38:06 +0200

    I dont remember the purpose of this line

--- files/helpers/media.py
+++ files/helpers/media.py
@@ -91,10 +91,9 @@ def process_video(file):
 def process_image(filename=None, resize=0):
 	size = os.stat(filename).st_size
 
-	if resize != 100:
-		if size > 16 * 1024 * 1024 or not patron and size > 8 * 1024 * 1024:
-			os.remove(filename)
-			abort(413)
+	if size > 16 * 1024 * 1024 or not patron and size > 8 * 1024 * 1024:
+		os.remove(filename)
+		abort(413)
 
 	i = Image.open(filename)
 
