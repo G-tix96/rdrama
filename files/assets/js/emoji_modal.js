73ff78d0f58d23a3cbc141fcd5403b41163dffd5
Aevann1 <randomname42029@gmail.com>
2022-09-12 06:38:04 +0200

    fix 1st part of this https://rdrama.org/post/18459/marseycapywalking-megathread-for-bugs-and-suggestions/2705204?context=8#context

--- files/assets/js/emoji_modal.js
+++ files/assets/js/emoji_modal.js
@@ -307,7 +307,7 @@ function switchEmojiTab(e)
 }
 
 async function start_search() {
-	emojiSearcher.addQuery(emojiSearchBarDOM.value.trim());
+	emojiSearcher.addQuery(emojiSearchBarDOM.value.trim().toLowerCase());
 
 	// Remove any selected tab, now it is meaningless
 	for(let i = 0; i < classesSelectorDOM.children.length; i++)
