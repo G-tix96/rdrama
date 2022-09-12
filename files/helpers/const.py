cce159dc84cc4b5c7dd166e7908909e83fdd975f
Aevann1 <randomname42029@gmail.com>
2022-09-12 07:01:37 +0200

    ping mccox whenever someone mentions his name

--- files/helpers/const.py
+++ files/helpers/const.py
@@ -181,6 +181,7 @@ BBBB_ID = 0
 
 SCHIZO_ID = 0
 KIPPY_ID = 0
+MCCOX_ID = 0
 PIZZASHILL_ID = 0
 GUMROAD_MESSY = ()
 PIZZA_VOTERS = ()
@@ -232,6 +233,7 @@ if SITE == 'rdrama.net':
 
 	SCHIZO_ID = 8494
 	KIPPY_ID = 7150
+	MCCOX_ID = 8239
 	PIZZASHILL_ID = 2424
 	GUMROAD_MESSY = (1230,1379)
 	PIZZA_VOTERS = (747,1963,9712)
@@ -839,18 +841,20 @@ NOTIFIED_USERS = {
 	'joan': JOAN_ID,
 	'pewkie': JOAN_ID,
 	'carp': CARP_ID,
-	'clit': CARP_ID,
 	'idio3': IDIO_ID,
 	'idio ': IDIO_ID,
-	'dong': DONGER_ID,
-	'kippy': KIPPY_ID,
 	'the_homocracy': HOMO_ID,
-	'soren': SOREN_ID,
 	'schizocel': SCHIZO_ID,
 	'scitzocel': SCHIZO_ID,
 	'snakes': SNAKES_ID,
 	'sneks': SNAKES_ID,
-	'geese': GEESE_ID
+	'geese': GEESE_ID,
+	'clit': CARP_ID,
+	'kippy': KIPPY_ID,
+	'mccox': MCCOX_ID,
+
+	'donger': DONGER_ID,
+	'soren': SOREN_ID,
 }
 
 FORTUNE_REPLIES = ('<b style="color:#6023f8">Your fortune: Allah Wills It</b>','<b style="color:#d302a7">Your fortune: Inshallah, Only Good Things Shall Come To Pass</b>','<b style="color:#e7890c">Your fortune: Allah Smiles At You This Day</b>','<b style="color:#7fec11">Your fortune: Your Bussy Is In For A Blasting</b>','<b style="color:#43fd3b">Your fortune: You Will Be Propositioned By A High-Tier Twink</b>','<b style="color:#9d05da">Your fortune: Repent, You Have Displeased Allah And His Vengeance Is Nigh</b>','<b style="color:#f51c6a">Your fortune: Reply Hazy, Try Again</b>','<b style="color:#00cbb0">Your fortune: lmao you just lost 100 coins</b>','<b style="color:#2a56fb">Your fortune: Yikes 😬</b>','<b style="color:#0893e1">Your fortune: You Will Be Blessed With Many Black Bulls</b>','<b style="color:#16f174">Your fortune: NEETmax, The Day Is Lost If You Venture Outside</b>','<b style="color:#fd4d32">Your fortune: A Taste Of Jannah Awaits You Today</b>','<b style="color:#bac200">Your fortune: Watch Your Back</b>','<b style="color:#6023f8">Your fortune: Outlook good</b>','<b style="color:#d302a7">Your fortune: Godly Luck</b>','<b style="color:#e7890c">Your fortune: Good Luck</b>','<b style="color:#7fec11">Your fortune: Bad Luck</b>','<b style="color:#43fd3b">Your fortune: Good news will come to you by mail</b>','<b style="color:#9d05da">Your fortune: Very Bad Luck</b>','<b style="color:#00cbb0">Your fortune: ｷﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━ !!!!</b>','<b style="color:#2a56fb">Your fortune: Better not tell you now</b>','<b style="color:#0893e1">Your fortune: You will meet a dark handsome stranger</b>','<b style="color:#16f174">Your fortune: （　´_ゝ`）ﾌｰﾝ</b>','<b style="color:#fd4d32">Your fortune: Excellent Luck</b>','<b style="color:#bac200">Your fortune: Average Luck</b>')
