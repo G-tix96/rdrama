49e5234503347dcbcc7edc79740eab314ad51042
Aevann1 <randomname42029@gmail.com>
2022-09-13 17:00:01 +0000

    sneed

--- schema.sql
+++ schema.sql
@@ -872,19 +872,6 @@ CREATE TABLE public.subscriptions (
 
 
 --
--- Name: transactions; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.transactions (
-    id integer NOT NULL,
-    created_utc integer NOT NULL,
-    type character varying(12) NOT NULL,
-    amount integer NOT NULL,
-    email character varying(255) NOT NULL
-);
-
-
---
 -- Name: userblocks; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -1477,14 +1464,6 @@ ALTER TABLE ONLY public.subscriptions
 
 
 --
--- Name: transactions transactions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.transactions
-    ADD CONSTRAINT transactions_pkey PRIMARY KEY (id);
-
-
---
 -- Name: users uid_unique; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -1967,13 +1946,6 @@ CREATE INDEX subs_idx ON public.subs USING btree (name);
 --
 
 CREATE INDEX subscription_user_index ON public.subscriptions USING btree (user_id);
-
-
---
--- Name: transactions_email_idx; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX transactions_email_idx ON public.transactions USING btree (email);
 
 
 --
