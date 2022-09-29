94f078982712a7229b23d6429796693a2e66c5f5
Aevann1 <randomname42029@gmail.com>
2022-09-29 12:21:55 +0200

    fix 500 error

--- files/helpers/awards.py
+++ files/helpers/awards.py
@@ -87,7 +87,7 @@ def award_timers(v, bot=False):
 		badge = v.has_badge(171)
 		if badge: g.db.delete(badge)
 	if v.spider and v.spider != 1 and v.spider < now:
-		v.spider = None
+		v.spider = 0
 		notify_if_not_bot("Your spider friend has left you!")
 		badge = v.has_badge(179)
 		if badge: g.db.delete(badge)
