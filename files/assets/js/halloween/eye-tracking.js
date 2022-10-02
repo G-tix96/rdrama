6d563f69b8fef09de63cce775ff81e93ee2ef0e3
gooseman <16601315+geese-suck@users.noreply.github.com>
2022-10-02 00:26:33 -0700

    MULTI BANNER ACTION

--- files/assets/js/halloween/eye-tracking.js
+++ files/assets/js/halloween/eye-tracking.js
@@ -9,52 +9,64 @@ const coordsLookup = {
 			x: "116px",
 			y: "-209px"
 		}	
+	}, 	
+	banner2: {
+		left: {
+			x: "35px",
+			y: "-335px"
+		},
+		right: {
+			x: "64px",
+			y: "-337px"
+		}	
 	}
 }
 
 let sidebar = document.getElementById("main-content-row")
-sidebar = sidebar.getElementsByClassName("sidebar")[0]
+sidebar = sidebar.getElementsByClassName("sidebar")[0].firstElementChild
 
-const banner = sidebar.firstElementChild
+const eye_left = initEye("left"),
+	  eye_right = initEye("right")
 
-const eye_left = document.createElement("img")
-eye_left.setAttribute("class", "eye")
-eye_left.setAttribute("src", "/assets/images/halloween/eye.png")
-eye_left.style.left = coordsLookup["banner1"]["left"]["x"]
-eye_left.style.top = coordsLookup["banner1"]["left"]["y"]
+function initEye(side) {
+	let eye = document.createElement("img")
 
-const eye_right = document.createElement("img")
-eye_right.setAttribute("class", "eye")
-eye_right.setAttribute("src", "/assets/images/halloween/eye.png")
-eye_right.style.left = coordsLookup["banner1"]["right"]["x"]
-eye_right.style.top = coordsLookup["banner1"]["right"]["y"]
-
-banner.appendChild(eye_left)
-banner.appendChild(eye_right)
+	eye.setAttribute("class", "eye")
+	eye.setAttribute("src", "/assets/images/halloween/eye.png")
+	eye.style.left = coordsLookup[banner][side]["x"]
+	eye.style.top = coordsLookup[banner][side]["y"]
+	
+	sidebar.appendChild(eye)
+	return(eye)
+}
 
 /*eye movement*/
-const eye_left_center = getCenter(eye_left)
-const eye_right_center = getCenter(eye_right)
+const eye_left_center = getCenter(eye_left),
+	  eye_right_center = getCenter(eye_right)
 
-document.onmousemove = function(event){
+document.onmousemove = function(event) {
 	let click = {x: event.clientX, y: event.clientY}
 	eye_left.style.transform = 'rotate('+getAngle(click, eye_left_center)+'rad)'
 	eye_right.style.transform = 'rotate('+getAngle(click, eye_right_center)+'rad)'
 }
 
-function getCenter(element){
+function getCenter(element) {
 	const bounds = element.getBoundingClientRect()
-	let centerX = Math.trunc(bounds["left"]) + Math.trunc(bounds["width"]/2)
-	let centerY = Math.trunc(bounds["top"]) + Math.trunc(bounds["height"]/2)
+
+	let centerX = bounds["left"] + bounds["width"]/2,
+		centerY = bounds["top"] + bounds["height"]/2
+
 	return({x: centerX, y: centerY})
 }
 
 function getAngle(point1, point2) {
-	let x = point1["x"] - point2["x"]
-	let y = point1["y"] - point2["y"]
-	let angle = Math.atan(y/x)
-	if(point1["x"] < point2["x"]){
+	let x = point1["x"] - point2["x"],
+		y = point1["y"] - point2["y"],
+		angle = Math.atan(y/x)
+
+	if(point1["x"] < point2["x"]) {
 		angle += Math.PI
 	}
+
 	return(angle)
 }
