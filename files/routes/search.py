216022a5a0e8cd3cc8ad28ab3025ceeb6f905ab4
TLSM <duolsm@outlook.com>
2022-10-02 06:24:03 -0400

    Comments search: utilize lexeme config.

--- files/routes/search.py
+++ files/routes/search.py
@@ -207,8 +207,9 @@ def searchcomments(v):
 		else: comments = comments.filter(Comment.author_id == author.id)
 
 	if 'q' in criteria:
-		comments = comments.filter(Comment.body_ts.op('@@')(
-			func.plainto_tsquery(' & '.join(criteria['q']))))
+		comments = comments.filter(Comment.body_ts.match(
+			' & '.join(criteria['q']),
+			postgresql_regconfig='english'))
 
 	if 'over18' in criteria: comments = comments.filter(Comment.over_18 == True)
 
