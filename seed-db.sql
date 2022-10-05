c7033559abc23eb2a49dc14091a967c8498c5ea4
Aevann1 <randomname42029@gmail.com>
2022-10-05 21:00:03 +0000

    sneed

--- seed-db.sql
+++ seed-db.sql
@@ -281,6 +281,8 @@ INSERT INTO public.hat_defs VALUES (781, 'BTC', 'Only wear this if you''re worth
 
 
 
+
+
 INSERT INTO public.hat_defs VALUES (723, 'Hohol', 'Мій предок :)', 2, 500, NULL, 1663892328);
 INSERT INTO public.hat_defs VALUES (750, 'Cave Man', 'UNGA BUNGA UNGA BUNGA OOO OOO', 2, 500, NULL, 1664595865);
 INSERT INTO public.hat_defs VALUES (753, 'Doom Guy', 'RIP AND TEAR', 2, 500, NULL, 1664595933);
@@ -936,7 +938,7 @@ INSERT INTO public.hat_defs VALUES (504, 'Iron Crown of Lombardy', 'This isn''t 
 -- Name: hat_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hat_defs_id_seq', 800, true);
+SELECT pg_catalog.setval('public.hat_defs_id_seq', 802, true);
 
 
 --
