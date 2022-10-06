e77a117339d936d9929b3e621b8e6dc1d2da5ad5
Aevann1 <randomname42029@gmail.com>
2022-10-06 07:37:50 +0200

    add created_utc column to streamers

--- files/classes/streamers.py
+++ files/classes/streamers.py
@@ -3,11 +3,17 @@ from files.helpers.const import SITE
 if SITE == 'pcmemes.net':
 	from sqlalchemy import *
 	from files.__main__ import Base
+	import time
 
 	class Streamer(Base):
 
 		__tablename__ = "streamers"
 		id = Column(String, primary_key=True)
+		created_utc = Column(Integer)
+
+		def __init__(self, *args, **kwargs):
+			if "created_utc" not in kwargs: kwargs["created_utc"] = int(time.time())
+			super().__init__(*args, **kwargs)
 
 		def __repr__(self):
 			return f"<Streamer(id={self.id})>"
