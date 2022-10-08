558ee805ecf3916c80dcf266bd1b8621483f35c8
justcool393 <justcool393@gmail.com>
2022-10-07 21:07:44 -0700

    re-add revert admin actions function and also fix a bug with it

--- files/helpers/const.py
+++ files/helpers/const.py
@@ -165,6 +165,7 @@ PERMS = { # Minimum admin_level to perform action.
 	'ADMIN_ADD': 3, # note: explicitly disabled on rDrama
 	'ADMIN_REMOVE': 3,
 	'ADMIN_ADD_PERM_LEVEL': 2, # permission level given when user added via site
+	'ADMIN_ACTIONS_REVERT': 3,
 	'ADMIN_MOP_VISIBLE': 2,
 	'ADMIN_HOME_VISIBLE': 2,
 	'DOMAINS_BAN': 3,
