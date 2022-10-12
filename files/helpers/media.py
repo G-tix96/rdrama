5377472146c7bb0d4c143bc733c12cb3810aec23
justcool393 <justcool393@gmail.com>
2022-10-12 01:53:23 -0700

    show correct max video size

--- files/helpers/media.py
+++ files/helpers/media.py
@@ -92,7 +92,7 @@ def process_video(file):
 	size = os.stat(old).st_size
 	if SITE_NAME != 'WPD' and (size > MAX_VIDEO_SIZE_MB * 1024 * 1024 or not g.v.patron and size > MAX_VIDEO_SIZE_MB_PATRON * 1024 * 1024):
 		os.remove(old)
-		abort(413, f"Max video size is {MAX_IMAGE_AUDIO_SIZE_MB} MB ({MAX_VIDEO_SIZE_MB_PATRON} MB for paypigs)")
+		abort(413, f"Max video size is {MAX_VIDEO_SIZE_MB} MB ({MAX_VIDEO_SIZE_MB_PATRON} MB for paypigs)")
 
 	extension = file.filename.split('.')[-1].lower()
 	if extension not in ['avi', 'mp4', 'webm', 'm4v', 'mov', 'mkv']:
