fb150dab8e8e8b3fec31b5b0bc290110deb8fcae
justcool393 <justcool393@gmail.com>
2022-10-13 00:58:33 -0700

    use constant instead of literal 5

--- files/helpers/cloudflare.py
+++ files/helpers/cloudflare.py
@@ -18,7 +18,7 @@ def _request_from_cloudflare(url:str, method:str, post_data_str) -> bool:
 def get_security_level() -> Optional[str]:
     res = None
     try:
-        res = requests.get(f'{CLOUDFLARE_API_URL}/zones/{CF_ZONE}/settings/security_level', headers=CF_HEADERS, timeout=5).json()['result']['value']
+        res = requests.get(f'{CLOUDFLARE_API_URL}/zones/{CF_ZONE}/settings/security_level', headers=CF_HEADERS, timeout=CLOUDFLARE_REQUEST_TIMEOUT_SECS).json()['result']['value']
     except:
         pass
     return res
