dca21741c3901ef60a0dbf5e3a4e571f8f79e66a
TLSM <duolsm@outlook.com>
2022-10-16 09:01:53 -0400

    Discord removal: fix syntax error, asst'd cleanup.

--- files/helpers/discord.py
+++ files/helpers/discord.py
@@ -2,13 +2,16 @@ import requests
 
 from .const import *
 
-headers = {"Authorization": f"Bot {DISCORD_BOT_TOKEN}"}
+def discord_message_send(channel_id, message):
+	requests.post(
+		f"https://discordapp.com/api/channels/{channel_id}/messages",
+		headers={"Authorization": f"Bot {DISCORD_BOT_TOKEN}"},
+		data={"content": message},
+		timeout=5)
 
 def send_changelog_message(message):
-	data={"content": message}
-	for id in DISCORD_CHANGELOG_CHANNEL_IDS:
-		requests.post(f"https://discordapp.com/api/channels/{id}/messages", headers=headers, data=data, timeout=5)
+	for channel_id in DISCORD_CHANGELOG_CHANNEL_IDS:
+		discord_message_send(channel_id, message)
 
 def send_wpd_message(message):
-	data={"content": message}
-	requests.post(f"https://discordapp.com/api/channels/{WPD_CHANNEL_ID}/messages", headers=headers, data=data, timeout=5)
+	discord_message_send(WPD_CHANNEL_ID, message)
