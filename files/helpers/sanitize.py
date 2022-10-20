3c3950adb013c1f9ec35e24bf3864933b3618578
Aevann1 <randomname42029@gmail.com>
2022-10-20 22:26:39 +0200

    change video tags

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -337,7 +337,7 @@ def sanitize(sanitized, golden=True, limit_pings=0, showmore=True, count_marseys
 
 		sanitized = sanitized.replace(i.group(0), htmlsource)
 
-	sanitized = video_sub_regex.sub(r'\1<video controls preload="none"><source src="\2"></video>', sanitized)
+	sanitized = video_sub_regex.sub(r'\1<video controls preload="none" src="\2"></video>', sanitized)
 	sanitized = audio_sub_regex.sub(r'\1<audio controls preload="none" src="\2"></audio>', sanitized)
 
 	if count_marseys:
