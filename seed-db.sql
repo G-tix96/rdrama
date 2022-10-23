c1b8829263a567b37a254fc2b2326e35dda071b1
Aevann1 <randomname42029@gmail.com>
2022-10-23 04:00:03 +0000

    sneed

--- seed-db.sql
+++ seed-db.sql
@@ -223,6 +223,7 @@ SET row_security = off;
 INSERT INTO public.hat_defs VALUES (716, 'Baked Beans Bucket Hat', 'This neighbor is wearing beans!', 2, 500, NULL, 1663647585);
 
 
+
 INSERT INTO public.hat_defs VALUES (718, 'Literally Me', 'Sving music, jazz? PTOOEY!', 2, 500, NULL, 1663801724);
 INSERT INTO public.hat_defs VALUES (719, 'The Matrix', 'The Matrix is an allegory of the transgender experience that you and everyone else got suckered into watching', 2, 1000, NULL, 1663836639);
 INSERT INTO public.hat_defs VALUES (720, 'The Matrix Resurrections', 'Wachowski moment', 2, 1000, NULL, 1663836967);
@@ -1079,7 +1080,7 @@ INSERT INTO public.hat_defs VALUES (911, 'Racecar Hat', 'I sleep in a hat with m
 -- Name: hat_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hat_defs_id_seq', 985, true);
+SELECT pg_catalog.setval('public.hat_defs_id_seq', 987, true);
 
 
 --
@@ -1974,6 +1975,7 @@ INSERT INTO public.marseys (name, author_id, tags, created_utc) VALUES
 ('marseykente',2,'black blm nigger nog jogger coon dindu bhm hotep kufi african tribal',NULL),
 ('marseykermit',2,'kermit green frog puppet absinthe television sesame children',NULL),
 ('marseykernelpanic',2,'linux kernel panic computer error glitch',NULL),
+('marseykevin',2,'marsey hat drseuss dark black',1666487566),
 ('marseykfc',2,'col sanders colonel kentucky fried chicken',NULL),
 ('marseykhorne',2,'warhammer 40k chaos god war death blood',NULL),
 ('marseykindness',2,'retarded special drooling idiot seriouspost srspost agendapost moron fool buffoon dramatard',NULL),
