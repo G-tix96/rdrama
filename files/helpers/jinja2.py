a9380414a9dd46836a920b013e1646a70828c381
TLSM <duolsm@outlook.com>
2022-10-24 01:13:13 -0400

    Improve infection meter denominator query.

--- files/helpers/jinja2.py
+++ files/helpers/jinja2.py
@@ -38,11 +38,12 @@ def bar_position():
 	db = db_session()
 	vaxxed = db.execute(text("SELECT COUNT(*) FROM users WHERE hw_zombie > 0")).one()[0]
 	zombie = db.execute(text("SELECT COUNT(*) FROM users WHERE hw_zombie < 0")).one()[0]
-	total = db.execute(text("SELECT COUNT(*) FROM (SELECT DISTINCT ON (author_id) "
-		"author_id FROM comments WHERE created_utc > 1666402200) AS q")).one()[0]
-
-	if total <= 0:
-		total = 1
+	total = db.execute(text("SELECT COUNT(*) FROM "
+		"(SELECT DISTINCT ON (author_id) author_id AS uid FROM comments "
+			"WHERE created_utc > 1666402200) AS q1 "
+		"FULL OUTER JOIN (SELECT id AS uid FROM users WHERE hw_zombie != 0) as q2 "
+		"ON q1.uid = q2.uid")).one()[0]
+	total = max(total, 1)
 
 	return [int((vaxxed * 100) / total), int((zombie * 100) / total)]
 
