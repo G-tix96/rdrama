2a05363a2ddb1324720bff051ded714530fb5f3a
Aevann1 <randomname42029@gmail.com>
2022-10-24 21:22:05 +0200

    exclude me from discord filter

--- files/routes/users.py
+++ files/routes/users.py
@@ -522,7 +522,7 @@ def message2(v, username):
 	message = sanitize_raw_body(request.values.get("message"), False)
 	if not message: abort(400, "Message is empty!")
 	if 'linkedin.com' in message: abort(403, "This domain 'linkedin.com' is banned.")
-	if 'discord.gg' in message or 'discord.com' in message or 'discordapp.com' in message:
+	if v.id != AEVANN_ID and ('discord.gg' in message or 'discord.com' in message or 'discordapp.com' in message):
 		abort(403, "Stop grooming!")
 
 	body_html = sanitize(message)
@@ -578,7 +578,7 @@ def messagereply(v):
 
 	if 'linkedin.com' in body: abort(403, "This domain 'linkedin.com' is banned")
 
-	if 'discord.gg' in body or 'discord.com' in body or 'discordapp.com' in body:
+	if v.id != AEVANN_ID and ('discord.gg' in body or 'discord.com' in body or 'discordapp.com' in body):
 		abort(403, "Stop grooming!")
 
 	id = request.values.get("parent_id")
