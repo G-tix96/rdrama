16820a2423ab1577e573b94a432f38b5e7e2c48e
Aevann1 <randomname42029@gmail.com>
2022-10-27 10:15:24 +0200

    make tracking eyes work for everyone on desktop (even poorcels)

--- files/assets/js/halloween/eye-tracking.js
+++ files/assets/js/halloween/eye-tracking.js
@@ -1,39 +1,42 @@
-/*init*/
-let sidebar = document.getElementById("main-content-row")
-sidebar = sidebar.getElementsByClassName("sidebar")[0].firstElementChild
+if (innerWidth >= 992)
+{
+	/*init*/
+	let sidebar = document.getElementById("main-content-row")
+	sidebar = sidebar.getElementsByClassName("sidebar")[0].firstElementChild
 
-const eye_left = sidebar.getElementsByClassName("eye")[0],
-	  eye_right = sidebar.getElementsByClassName("eye")[1]
+	const eye_left = sidebar.getElementsByClassName("eye")[0],
+		eye_right = sidebar.getElementsByClassName("eye")[1]
 
-/*eye movement*/
-document.onmousemove = function(event) {
-	let click = {x: event.clientX, y: event.clientY},
-		/*recalculating eye position on every mousemove is less efficient
-		but ensures eyes track properly upon pageload even if script doesnt fire*/
-		eye_left_center = getCenter(eye_left),
-		eye_right_center = getCenter(eye_right)
+	/*eye movement*/
+	document.onmousemove = function(event) {
+		let click = {x: event.clientX, y: event.clientY},
+			/*recalculating eye position on every mousemove is less efficient
+			but ensures eyes track properly upon pageload even if script doesnt fire*/
+			eye_left_center = getCenter(eye_left),
+			eye_right_center = getCenter(eye_right)
 
-	eye_left.style.transform = 'rotate('+getAngle(click, eye_left_center)+'rad)'
-	eye_right.style.transform = 'rotate('+getAngle(click, eye_right_center)+'rad)'
-}
-
-function getCenter(element) {
-	const bounds = element.getBoundingClientRect()
-
-	let centerX = bounds["left"] + bounds["width"]/2,
-		centerY = bounds["top"] + bounds["height"]/2
-
-	return({x: centerX, y: centerY})
-}
-
-function getAngle(point1, point2) {
-	let x = point1["x"] - point2["x"],
-		y = point1["y"] - point2["y"],
-		angle = Math.atan(y/x)
-
-	if(point1["x"] < point2["x"]) {
-		angle += Math.PI
+		eye_left.style.transform = 'rotate('+getAngle(click, eye_left_center)+'rad)'
+		eye_right.style.transform = 'rotate('+getAngle(click, eye_right_center)+'rad)'
 	}
 
-	return(angle)
+	function getCenter(element) {
+		const bounds = element.getBoundingClientRect()
+
+		let centerX = bounds["left"] + bounds["width"]/2,
+			centerY = bounds["top"] + bounds["height"]/2
+
+		return({x: centerX, y: centerY})
+	}
+
+	function getAngle(point1, point2) {
+		let x = point1["x"] - point2["x"],
+			y = point1["y"] - point2["y"],
+			angle = Math.atan(y/x)
+
+		if(point1["x"] < point2["x"]) {
+			angle += Math.PI
+		}
+
+		return(angle)
+	}
 }
