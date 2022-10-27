c2572fe2ac435937637d261b35c31f013c0b43cb
justcool393 <justcool393@gmail.com>
2022-10-27 12:35:46 -0500

    hosts: disable multi host feature

--- files/helpers/hosts.py
+++ files/helpers/hosts.py
@@ -1,8 +1,12 @@
 from .const import *
 
 def current_host():
-	if SITE == "localhost": prefix = "http://"
-	else: prefix = "https://"
+	# NB: apparently this is pretty broken in some cases so we're going
+    # to just use SITE_FULL until it's figured out
+	return SITE_FULL
+
+	# if SITE == "localhost": prefix = "http://"
+	# else: prefix = "https://"
 	
-	if request.host not in SITE_HOSTS: return SITE_FULL
-	return prefix + request.host
+	# if request.host not in SITE_HOSTS: return SITE_FULL
+	# return prefix + request.host
