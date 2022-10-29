c4063f081c2750ac137794eab6cba0b45b36082d
Aevann1 <randomname42029@gmail.com>
2022-10-30 00:53:42 +0200

    moved boosted sites to const.py

--- files/routes/votes.py
+++ files/routes/votes.py
@@ -139,54 +139,7 @@ def vote_post_comment(target_id, new, v, cls, vote_cls):
 	target.downvotes = get_vote_count(-1, False)
 	target.realupvotes = get_vote_count(0, True) # first arg is ignored here
 
-	boost_sites = {
-		'rdrama.net',
-		'pcmemes.net',
-		'watchpeopledie.tv',
-		'quora.com',
-		'cumtown.org',
-		'notabug.io',
-		'talk.lol',
-		'discussions.app',
-		'gab.com',
-		'kiwifarms.net',
-		'gettr.com',
-		'scored.co',
-		'parler.com',
-		'bitchute.com',
-		'4chan.org',
-		'givesendgo.com',
-		'thepinkpill.com',
-		'ovarit.com',
-		'rdrama.cc',
-		'lolcow.farm',
-		'truthsocial.com',
-		'rumble.com',
-		'saidit.net',
-		'kiwifarms.cc',
-		'8kun.top',
-		'goyimtv.tv',
-		'poal.co',
-		'stormfront.org',
-		'arete.network',
-		'poa.st',
-		'lbry.com',
-		'crystal.cafe',
-		'tribel.com',
-		'mstdn.social',
-		'mastodon.online',
-		'steemit.com',
-		'hexbear.net',
-		'raddle.me',
-		'lemmy.ml',
-		'bluelight.org',
-		'incels.is',
-		'groups.google.com',
-		't.me',
-		'web.telegram.org'
-	}
-
-	if target.author.progressivestack or (cls == Submission and (target.sub in ('space', 'istory', 'dinos') or target.domain.endswith('.win') or target.domain in boost_sites)):
+	if target.author.progressivestack or (cls == Submission and (target.sub in ('space', 'istory', 'dinos') or target.domain.endswith('.win') or target.domain in BOOSTED_SITES)):
 		target.realupvotes *= 2
 	g.db.add(target)
 	return "", 204
