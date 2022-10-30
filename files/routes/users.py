d1e35e4afed64c425147f905594ea6342fccd043
justcool393 <justcool393@gmail.com>
2022-10-30 02:37:31 -0500

    fix prev commit

--- files/routes/users.py
+++ files/routes/users.py
@@ -661,15 +661,13 @@ def u_username(username, v=None):
 	if not u.is_visible_to(v):
 		if g.is_api_or_xhr or request.path.endswith(".json"):
 			abort(403, "This userpage is private")
-
-		return render_template("userpage_private.html", u=u, v=v)
+		return render_template("userpage_private.html", u=u, v=v), 403
 
 	
 	if v and hasattr(u, 'is_blocking') and u.is_blocking:
 		if g.is_api_or_xhr or request.path.endswith(".json"):
 			abort(403, f"You are blocking @{u.username}.")
-
-		return render_template("userpage_blocking.html", u=u, v=v)
+		return render_template("userpage_blocking.html", u=u, v=v), 403
 
 
 	sort = request.values.get("sort", "new")
