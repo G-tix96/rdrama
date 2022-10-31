b8542af64ceb55ff84deab3113d48479767681ad
justcool393 <justcool393@gmail.com>
2022-10-30 20:10:33 -0500

    random fix

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -209,7 +209,8 @@ def sanitize(sanitized, golden=True, limit_pings=0, showmore=True, count_marseys
 
 	if torture:
 		sanitized = torture_ap(sanitized, g.v.username)
-		sanitized += '\n:#trumpjaktalking:' if random.random() >= 0.50 else '\n:#reposthorse:'
+		emoji = choice(['trumpjaktalking', 'reposthorse'])
+		sanitized += f'\n:#{emoji}:'
 
 	sanitized = normalize_url(sanitized)
 
