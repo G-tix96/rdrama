32f079ed204d3b3450b7ea12a8c6212068bd1128
Aevann1 <randomname42029@gmail.com>
2022-11-03 17:38:46 +0200

    make post approval and removal effects only happen when response code is between 200 and 300

--- files/assets/js/submission_admin.js
+++ files/assets/js/submission_admin.js
@@ -1,31 +1,56 @@
 function removePost(t,post_id,button1,button2,cls) {
 	url="/remove_post/"+post_id
 
-	postToast(t,url,button1,button2,cls)
+	t.disabled = true;
+	t.classList.add("disabled");
+	postToast_callback(url,
+		{
+		},
+		(xhr) => {
+			if (xhr.status >= 200 && xhr.status < 300) {
+				if (window.location.pathname == '/admin/reported/posts')
+				{
+					document.getElementById("flaggers-"+post_id).remove()
+					document.getElementById("post-"+post_id).remove()
+				}
+				else
+				{
+					document.getElementById("post-"+post_id).classList.add("banned");
+					document.getElementById(button1).classList.toggle(cls);
+					document.getElementById(button2).classList.toggle(cls);	
+				}
+			}
+			t.disabled = false;
+			t.classList.remove("disabled");		
+		}
+	);
+}
 
-	if (window.location.pathname == '/admin/reported/posts')
-	{
-		document.getElementById("flaggers-"+post_id).remove()
-		document.getElementById("post-"+post_id).remove()
-	}
-	else
-	{
-		document.getElementById("post-"+post_id).classList.add("banned");
-	}
-};
 
 function approvePost(t,post_id,button1,button2,cls) {
 	url="/approve_post/"+post_id
 
-	postToast(t,url,button1,button2,cls)
-
-	if (window.location.pathname == '/admin/reported/posts')
-	{
-		document.getElementById("flaggers-"+post_id).remove()
-		document.getElementById("post-"+post_id).remove()
-	}
-	else
-	{
-		document.getElementById("post-"+post_id).classList.remove("banned");
-	}
+	t.disabled = true;
+	t.classList.add("disabled");
+	postToast_callback(url,
+		{
+		},
+		(xhr) => {
+			if (xhr.status >= 200 && xhr.status < 300) {
+				if (window.location.pathname == '/admin/reported/posts')
+				{
+					document.getElementById("flaggers-"+post_id).remove()
+					document.getElementById("post-"+post_id).remove()
+				}
+				else
+				{
+					document.getElementById("post-"+post_id).classList.remove("banned");
+					document.getElementById(button1).classList.toggle(cls);
+					document.getElementById(button2).classList.toggle(cls);
+				}
+			}
+			t.disabled = false;
+			t.classList.remove("disabled");		
+		}
+	);
 }
