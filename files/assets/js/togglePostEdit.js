5b274c95c9ccb23c2d055082730b05507e972ea2
Aevann1 <randomname42029@gmail.com>
2022-11-11 16:47:51 +0200

    expand post-title field when editing

--- files/assets/js/togglePostEdit.js
+++ files/assets/js/togglePostEdit.js
@@ -2,10 +2,13 @@ function togglePostEdit(id){
 	body=document.getElementById("post-body");
 	title=document.getElementById("post-title");
 	form=document.getElementById("edit-post-body-"+id);
-	box=document.getElementById("post-edit-box-"+id);
 
 	body.classList.toggle("d-none");
 	title.classList.toggle("d-none");
 	form.classList.toggle("d-none");
+
+	box=document.getElementById("post-edit-box-"+id);
+	autoExpand(box);
+	box=document.getElementById("post-edit-title");
 	autoExpand(box);
 };
