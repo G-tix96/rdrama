eca45ecf1689b86652ad713dfd7560211e6d3464
Aevann1 <randomname42029@gmail.com>
2022-11-12 16:47:12 +0200

    update app url

--- files/helpers/const.py
+++ files/helpers/const.py
@@ -1520,7 +1520,7 @@ tiers={
 
 has_sidebar = path.exists(f'files/templates/sidebar_{SITE_NAME}.html')
 has_logo = path.exists(f'files/assets/images/{SITE_NAME}/logo.webp')
-has_app = path.exists(f'files/assets/app_{SITE_NAME}_v2.5.apk')
+has_app = path.exists(f'files/assets/app_{SITE_NAME}_v2.6.apk')
 
 ONLINE_STR = f'{SITE}_online'
 
