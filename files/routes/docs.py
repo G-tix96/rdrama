dea8c3f7e374d06fe4f12dbb0c92f30d2882045e
Nekobit Bitneko <me@ow.nekobit.net>
2022-11-25 23:30:49 -0500

    docs: Remove unneccesary imports; @auth_desired

--- files/routes/docs.py
+++ files/routes/docs.py
@@ -1,35 +1,13 @@
-import os
-from collections import Counter
-from json import loads
-from shutil import copyfile
 import xml
-
-import gevent
-
-from files.classes import *
-from files.helpers.actions import *
-from files.helpers.alerts import *
-from files.helpers.cloudflare import purge_files_in_cache
-from files.helpers.const import *
-from files.helpers.get import *
-from files.helpers.marsify import marsify
-from files.helpers.media import *
-from files.helpers.owoify import owoify
-from files.helpers.regex import *
-from files.helpers.sanitize import filter_emojis_only
-from files.helpers.slots import *
-from files.helpers.treasure import *
-from files.routes.front import comment_idlist
-from files.routes.routehelpers import execute_shadowban_viewers_and_voters
-from files.routes.wrappers import *
-from files.__main__ import app, cache, limiter
-
 import xml.etree.ElementTree as ET
+from files.__main__ import app, cache, limiter
 
 tree = ET.parse('docs/api.xml')
 
 @app.get("/dev/api")
+@auth_desired
 def get_docs_page():
+    # TODO cache
     root = tree.getroot()
 
     return render_template("docs.html", root=tree.getroot())
