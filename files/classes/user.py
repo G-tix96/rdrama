39bb04bdb8a7e036590d60cfe42ac0fbf737f4cc
Aevann1 <randomname42029@gmail.com>
2022-11-26 22:10:40 +0200

    remove TS limit for !YOU!

--- files/classes/user.py
+++ files/classes/user.py
@@ -1000,7 +1000,7 @@ class User(Base):
 			if not cls.can_see(user, other.author): return False
 			if user and user.id == other.author_id: return True
 			if isinstance(other, Submission):
-				if "!YOU!" in other.title and not (user and (user.client or user.truescore > 5000)): return False
+				if "!YOU!" in other.title and not user: return False
 				if other.sub and not cls.can_see(user, other.subr): return False
 			else:
 				if not other.parent_submission:
