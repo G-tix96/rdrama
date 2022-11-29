d6694226ecab058e7135b092984061c8f72596d2
justcool393 <justcool393@gmail.com>
2022-11-29 14:48:10 -0600

    events: remove lazy, fix __repr__, and rename table

--- files/events/classes/eventuser.py
+++ files/events/classes/eventuser.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import relationship
 from files.classes import Base
 
 class EventUser(Base):
-	__tablename__ = "event"
+	__tablename__ = "event_users"
 	id = Column(Integer, ForeignKey("users.id"), primary_key=True)
-	user = relationship("User", primaryjoin="EventUser.id == User.id", lazy="joined")
+	user = relationship("User", primaryjoin="EventUser.id == User.id")
 	event_music = Column(Boolean, default=True, nullable=False)
 
 	# start event specific columns
@@ -17,4 +17,4 @@ class EventUser(Base):
 		super().__init__(*args, **kwargs)
 
 	def __repr__(self):
-		return f"<Event(id={self.id})>"
+		return f"<{self.__class__.__name__}(id={self.id})>"
