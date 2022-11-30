497c9bcff1c47da6575ac82a33ada22ab4a40140
justcool393 <justcool393@gmail.com>
2022-11-30 12:49:22 -0600

    logging: don't use \n in f-string

--- files/helpers/logging.py
+++ files/helpers/logging.py
@@ -1,12 +1,12 @@
 from files.helpers.const import LOG_DIRECTORY
 
-def log_file(log_str:str, log_filename="rdrama.log", append_newline=True):
+def log_file(log_str:str, log_filename="rdrama.log"):
 	'''
 	Simple method to log a string to a file
 	'''
 	log_target = f"{LOG_DIRECTORY}/{log_filename}"
 	try:
 		with open(log_target, "a", encoding="utf-8") as f:
-			f.write(f"{log_str}{'\n' if append_newline else ''}")
+			f.write(log_str + '\n')
 	except Exception as e:
 		print(f"Failed to log to file {log_target} due to {e.__class__.__name__}")
