5b7572af642daf531f139e499d6098e0aa12a0ce
Snakes <duolsm@outlook.com>
2022-11-30 16:10:20 -0500

    Revert "blizzard submodule"
    This reverts commit eae0715c10965207f7527354aa5413ce8877bdd3.
    
    Sorry @geese_suck, looks like more of a pain to deploy than I thought
    it'd be. Was a good idea though.

---
+++ files/events/helpers/get.py
@@ -0,0 +1,15 @@
+from typing import Optional, Union
+from sqlalchemy.orm import scoped_session
+
+from files.events.classes.eventuser import EventUser
+from files.classes.user import User
+
+def get_or_create_event_user(target:Union[int, User], db:scoped_session) -> Optional[EventUser]:
+	if target is None: return None
+	if isinstance(target, User): target = target.id  # type: ignore
+	user = db.get(EventUser, target)
+	if not user:
+		user = EventUser(id=target)
+		db.add(user)
+		db.flush()
+	return user
