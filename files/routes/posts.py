3308734dc777829a2f88c45d52c5c65f64b2bdd9
Aevann1 <randomname42029@gmail.com>
2022-12-01 18:22:33 +0200

    dont auto-pin if post sub is stealthed

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -924,7 +924,7 @@ def submit_post(v:User, sub=None):
 
 	execute_lawlz_actions(v, post)
 
-	if SITE == 'rdrama.net' and v.id in (IMPASSIONATA_ID, PIZZASHILL_ID, 2008):
+	if SITE == 'rdrama.net' and not post.sub.stealth and v.id in (IMPASSIONATA_ID, PIZZASHILL_ID, 2008):
 		post.stickied_utc = int(time.time()) + 3600
 		post.stickied = "AutoJanny"
 
