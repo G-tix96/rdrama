d8292b7601b9f3e314da0d4139c76cf9a8e92d43
Aevann1 <randomname42029@gmail.com>
2022-12-03 01:47:35 +0200

    try different approach to opening push notification

--- files/assets/js/service_worker.js
+++ files/assets/js/service_worker.js
@@ -70,7 +70,9 @@ self.addEventListener('push', function(event) {
 });
 
 self.addEventListener('notificationclick', (e) => {
-	if (e.notification.data.url)
-		e.waitUntil(clients.openWindow(e.notification.data.url));
 	e.notification.close();
-});
+	e.waitUntil(clients.matchAll({ type: 'window' }).then((clientsArr) => {
+		const hadWindowToFocus = clientsArr.some((windowClient) => windowClient.url === e.notification.data.url ? (windowClient.focus(), true) : false);
+		if (!hadWindowToFocus) clients.openWindow(e.notification.data.url).then((windowClient) => windowClient ? windowClient.focus() : null);
+	}));
+});	
