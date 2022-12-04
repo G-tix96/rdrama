3eecec0a51f5e31e10db23aa0dc174ebcb8f8a94
Aevann <aevann@noreply.fsdfsd.net>
2022-12-04 19:02:22 +0000

    downloadable videos on apple devices (#42)
    ![image](/attachments/bd1b3e1e-c0a9-4eee-9b1c-a03e1e8a8cbd)
    
    Co-authored-by: Aevann1 <randomname42029@gmail.com>
    Co-authored-by: Snakes <duolsm@outlook.com>
    Co-authored-by: Snakes <snakes@noreply.fsdfsd.net>
    Reviewed-on: https://fsdfsd.net/rDrama/rDrama/pulls/42
    Co-authored-by: Aevann <aevann@noreply.fsdfsd.net>
    Co-committed-by: Aevann <aevann@noreply.fsdfsd.net>

--- files/classes/user.py
+++ files/classes/user.py
@@ -1016,12 +1016,15 @@ class User(Base):
 		Whether a user can strictly see this item. can_see_content is used where
 		content of a thing can be hidden from view
 		'''
+		browser = g and g.browser # TODO: request state object to pass to models for purity
 		if isinstance(other, (Submission, Comment)):
 			if not cls.can_see(user, other.author): return False
 			if user and user.id == other.author_id: return True
 			if isinstance(other, Submission):
 				if "!YOU!" in other.title and not user: return False
-				if not g.webview and other.author_id == AEVANN_ID and other.title.startswith('[ANDROID]'):
+				if browser != 'webview' and other.author_id == AEVANN_ID and other.title.startswith('[ANDROID]'):
+					return False
+				if browser != 'apple' and other.author_id == CARP_ID and other.title.startswith('[APPLE]'):
 					return False
 				if other.sub and not cls.can_see(user, other.subr): return False
 			else:
