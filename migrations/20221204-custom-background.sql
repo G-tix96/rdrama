ac26c2581dd5d35c3a418da67ae907a176e6a779
Snakes <duolsm@outlook.com>
2022-12-04 19:06:44 -0500

    Update custom-bg migration to support data.

--- migrations/20221204-custom-background.sql
+++ migrations/20221204-custom-background.sql
@@ -1,2 +1,2 @@
-alter table users alter column background type varchar(30);
+alter table users alter column background type varchar(40); -- solarpunk/xx.webp ones are 32 chars, plus leave headroom
 update users set background='/i/backgrounds/' || background || '.webp' where background is not null;
