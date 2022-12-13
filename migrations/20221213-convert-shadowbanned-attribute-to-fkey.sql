057f40469cc23bf8b807523c421173cbc5480ee4
Aevann1 <randomname42029@gmail.com>
2022-12-13 21:03:14 +0200

    minor bug fix

--- migrations/20221213-convert-shadowbanned-attribute-to-fkey.sql
+++ migrations/20221213-convert-shadowbanned-attribute-to-fkey.sql
@@ -1,3 +1,4 @@
+update users u set shadowbanned=(select id from users f where username=u.shadowbanned) where shadowbanned is not null;
 alter table users alter column shadowbanned type int using shadowbanned::integer;
 create index fki_user_shadowbanned_fkey on public.users using btree (shadowbanned);
 alter table only public.users add constraint user_shadowbanned_fkey foreign key (shadowbanned) references public.users(id);
