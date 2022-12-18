4a334133bcbfd8fa4e0dfd4da23ab2e17ba6e9ac
Aevann1 <randomname42029@gmail.com>
2022-12-18 18:47:11 +0200

    same as last commit

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -522,7 +522,7 @@ def badge_grant_get(v):
 
 @app.post("/admin/badge_grant")
 @feature_required('BADGES')
-@limiter.limit("500/day") #temporary
+@limiter.limit("700/day") #temporary
 @admin_level_required(PERMS['USER_BADGES'])
 def badge_grant_post(v):
 	badges = admin_badges_grantable_list(v)
