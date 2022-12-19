98afe7d2c34b06bdba14df0b999b9f17dc4c8a6c
Aevann1 <randomname42029@gmail.com>
2022-12-19 18:15:25 +0200

    don't use linux symlinks so the site works in windows

--- files/helpers/assetcache.py
+++ files/helpers/assetcache.py
@@ -13,7 +13,7 @@ ASSET_URL = '/assets/'
 ASSET_CACHE = defaultdict(lambda: None)
 
 if HOLIDAY_EVENT:
-	ASSET_SUBDIRS.extend(['/css/event', '/js/event'])
+	ASSET_SUBDIRS.extend(['/event/css', '/event/js'])
 
 def assetcache_build(asset_dir, subdirs):
 	for subdir in subdirs:
