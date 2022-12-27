219482c745e5a926125a3546d2cec7f79d82d52c
Aevann <randomname42029@gmail.com>
2022-12-27 04:17:15 +0200

    remove unnecessary dict entry

--- files/events/helpers/jinja.py
+++ files/events/helpers/jinja.py
@@ -10,7 +10,6 @@ EVENT_JINJA_CONST = {
 	"EVENT_ICONS": True,
 	"EVENT_STYLES": "blizzard.css",
 	"EVENT_AWARDS": True,
-	"EVENT_MUSIC": "music.html",
 	"EVENT_VISITORS_HERE_FLAVOR": [
 		' santa enjoyers kissing under a misletoe',
 		' bringing up family drama at Christmas dinner',
