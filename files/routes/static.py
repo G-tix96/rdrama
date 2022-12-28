c326c999f272e8cbef062bba194b3d7f5943158f
Aevann <randomname42029@gmail.com>
2022-12-28 12:25:52 +0200

    fix csp violation

--- files/routes/static.py
+++ files/routes/static.py
@@ -335,11 +335,14 @@ def donate(v):
 	return render_template(f'donate_{SITE_NAME}.html', v=v)
 
 
+items_we_want = ('blocked-uri', 'document-uri', 'effective-directive', 'source-file', 'violated-directive')
+
 @app.post('/csp_violations')
 @limiter.limit("10/minute;50/day")
 def csp_violations():
-	content = request.get_json(force=True)
-	content = str(json.dumps(content, indent=4, sort_keys=True))
-	if f'"source-file": "{SITE_FULL}' in content:
-		print(content, flush=True)
+	content = json.dumps(request.get_json())
+	if content["source-file"].startswith(SITE_FULL):
+		print('--------', flush=True)
+		for i in items_we_want:
+			print(f"{i}: {content['i']}")
 	return ''
