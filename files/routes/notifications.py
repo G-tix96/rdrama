04c91082a5d47a85f1e73e4a81bf30104615363d
Aevann <randomname42029@gmail.com>
2022-12-28 11:37:55 +0200

    also show me "unshadowban" and "unban_user"

--- files/routes/notifications.py
+++ files/routes/notifications.py
@@ -193,7 +193,7 @@ def notifications_modactions(v:User):
 	listing = g.db.query(cls).filter(cls.user_id != v.id)
 
 	if v.id == AEVANN_ID and SITE_NAME == 'rDrama':
-		listing = listing.filter(cls.kind.in_(('ban_user','shadowban')))
+		listing = listing.filter(cls.kind.in_(('ban_user','unban_user','shadowban','unshadowban')))
 
 	if cls == SubAction:
 		listing = listing.filter(cls.sub.in_(v.moderated_subs))
