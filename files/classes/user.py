0f4de38141ab7f3f977c504dc9c1ff6bc8b8e4fa
Aevann <randomname42029@gmail.com>
2023-01-24 07:20:15 +0200

    exclude merari and permabanned/shadowbanned from chudrama

--- files/classes/user.py
+++ files/classes/user.py
@@ -977,6 +977,9 @@ class User(Base):
 	@property
 	@lazy
 	def can_see_chudrama(self):
+		if self.id == 2074: return False
+		if self.shadowbanned: return False
+		if self.is_suspended_permanently: return False
 		if self.admin_level >= PERMS['VIEW_CHUDRAMA']: return True
 		if self.client: return True
 		if self.truescore >= TRUESCORE_CHUDRAMA_MINIMUM: return True
