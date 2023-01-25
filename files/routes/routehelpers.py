e1f4d53b9a673cb2f55e101450fa2abf2f6c6874
Aevann <randomname42029@gmail.com>
2023-01-25 07:50:22 +0200

    exclude me from alt linking

--- files/routes/routehelpers.py
+++ files/routes/routehelpers.py
@@ -45,6 +45,8 @@ def get_alt_graph(uid:int) -> List[User]:
 	return g.db.query(User).filter(User.id.in_(alt_ids)).order_by(User.username).all()
 
 def add_alt(user1:int, user2:int):
+	if user1 == AEVANN_ID or user2 == AEVANN_ID:
+		return
 	li = [user1, user2]
 	existing = g.db.query(Alt).filter(Alt.user1.in_(li), Alt.user2.in_(li)).one_or_none()
 	if not existing:
