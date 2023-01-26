d6dda995afaaffaa0ffef9e15bfc6943790c68bd
Aevann <randomname42029@gmail.com>
2023-01-26 07:47:20 +0200

    fix cartesian product

--- files/routes/notifications.py
+++ files/routes/notifications.py
@@ -201,7 +201,7 @@ def notifications_modactions(v:User):
 		listing = listing.filter(cls.kind.in_(('ban_user','unban_user','shadowban','unshadowban')))
 
 	if v.admin_level < PERMS['PROGSTACK']:
-		listing = listing.filter(ModAction.kind.notin_(MODACTION_PRIVILEGED__TYPES))
+		listing = listing.filter(cls.kind.notin_(MODACTION_PRIVILEGED__TYPES))
 
 	if cls == SubAction:
 		listing = listing.filter(cls.sub.in_(v.moderated_subs))
