2854599ed57cc6f7fd8f246b3e4db7ade7ebab8d
Aevann <randomname42029@gmail.com>
2023-01-27 10:19:36 +0200

    fix this https://rdrama.net/post/18459/marseycapywalking-megathread-for-bugs-and-suggestions/3489058#context

--- files/routes/users.py
+++ files/routes/users.py
@@ -845,11 +845,6 @@ def u_username_wall_comment(v:User, username:str, cid):
 
 	is_following = v and u.has_follower(v)
 
-	if not u.is_visible_to(v):
-		if g.is_api_or_xhr or request.path.endswith(".json"):
-			abort(403, f"@{u.username}'s userpage is private")
-		return render_template("userpage/private.html", u=u, v=v, is_following=is_following), 403
-
 	if v and v.id != u.id and not v.admin_level:
 		g.db.flush()
 		view = g.db.query(ViewerRelationship).filter_by(viewer_id=v.id, user_id=u.id).one_or_none()
