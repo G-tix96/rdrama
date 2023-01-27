1d6209e5bc99dd679382eb5458fc953577f137a1
Aevann <randomname42029@gmail.com>
2023-01-27 15:06:37 +0200

    fix this https://stupidpol.site/post/18459/marseycapywalking-megathread-for-bugs-and-suggestions/3419331#context

--- files/assets/js/userpage.js
+++ files/assets/js/userpage.js
@@ -2,5 +2,5 @@ function badge_timestamp(t) {
 	const date = formatDate(new Date(t.dataset.until*1000));
 	const text = t.getAttribute("data-bs-original-title")
 	t.setAttribute("data-bs-original-title", `${text} ${date.toString()}`);
-	t.removeAttribute("onmouseover")
+	t.removeAttribute("data-onmouseover")
 }
