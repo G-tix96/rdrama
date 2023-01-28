7beadcee7147b78a28ccef94f61210d5547a75c9
Aevann <randomname42029@gmail.com>
2023-01-28 13:33:51 +0200

    move snow.js to a vendor folder

--- files/helpers/assetcache.py
+++ files/helpers/assetcache.py
@@ -13,7 +13,7 @@ ASSET_URL = '/assets/'
 ASSET_CACHE = defaultdict(lambda: None)
 
 if IS_FISTMAS():
-	ASSET_SUBDIRS.extend(['/fistmas/css', '/fistmas/css/themes', '/fistmas/js'])
+	ASSET_SUBDIRS.extend(['/fistmas/css', '/fistmas/css/themes', '/fistmas/js', '/fistmas/js/vendor'])
 
 def assetcache_build(asset_dir, subdirs):
 	for subdir in subdirs:
