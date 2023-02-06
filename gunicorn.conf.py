cae748c2bc27dde7dd51b2985fe1b51d344f3169
Aevann <randomname42029@gmail.com>
2023-02-06 05:40:56 +0200

    make gunicorn warnings more prominent

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -13,14 +13,14 @@ def worker_abort(worker):
 	try:
 		from flask import g, request
 		if g and request:
-			worker.log.warning(f"\n\n★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★")
+			worker.log.warning(f"\n\n★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★")
 			worker.log.warning(f"While serving {request.method} {request.url}")
 			u = getattr(g, 'v', None)
 			if u:
 				worker.log.warning(f"User: {u.username!r} id:{u.id}")
 			else:
 				worker.log.warning(f"User: not logged in")
-			worker.log.warning(f"\n★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n")
+			worker.log.warning(f"\n★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n")
 		else:
 			worker.log.warning("No request info")
 	except:
